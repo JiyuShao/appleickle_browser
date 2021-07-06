bfb3cdbdf5eeb92ef80f4f05afeb8e1d812f907f
Jiyu Shao <jiyu.shao@gmail.com>
2021-07-06 19:55:34 +0800

    feat: 🎸 add search screen navigation animation

--- lib/widgets/search_bar/search_bar.dart
+++ lib/widgets/search_bar/search_bar.dart
@@ -3,15 +3,37 @@
  * @Author: Jiyu Shao 
  * @Date: 2021-07-02 17:39:20 
  * @Last Modified by: Jiyu Shao
- * @Last Modified time: 2021-07-05 10:37:53
+ * @Last Modified time: 2021-07-06 19:51:50
  */
 import 'package:flutter/material.dart';
+import 'package:pickle_browser/models/app_theme.dart';
+import 'package:pickle_browser/utils/debounce.dart';
+
+enum SearchBarKeyboardStatus {
+  // 打开中
+  opening,
+  // 已打开
+  opened,
+  // 关闭中
+  closing,
+  // 已关闭
+  closed,
+}
 
 class SearchBar extends StatefulWidget {
   // 是否启用输入框
   final bool enabled;
+  // 是否自动聚焦
+  final bool autofocus;
+  // 键盘显示回调事件
+  final void Function(SearchBarKeyboardStatus keyboardStatus)?
+      handleKeyboardChange;
 
-  SearchBar({Key? key, this.enabled = true})
+  SearchBar(
+      {Key? key,
+      this.enabled = true,
+      this.autofocus = false,
+      this.handleKeyboardChange})
       : super(
           key: key,
         );
@@ -20,31 +42,72 @@ class SearchBar extends StatefulWidget {
   _SearchBarState createState() => _SearchBarState();
 }
 
-class _SearchBarState extends State<SearchBar> {
+class _SearchBarState extends State<SearchBar> with WidgetsBindingObserver {
   // setState 之后防止键盘关闭, 设置 key
-  late GlobalKey<FormState> _formKey;
+  late final GlobalKey<FormState> _formKey;
   // 接收是否 focus 数据
-  late FocusNode _focusNode;
+  late final FocusNode _focusNode;
+  // debounce 使用的 key
+  late final Symbol _debounceKey;
+  // 键盘距离底部缓存
+  double _lastKeyboardBottom = 0;
+  // 键盘是否已打开缓存
+  bool _lastIsKeyboardOpen = false;
 
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance!.addObserver(this);
     _formKey = new GlobalKey<FormState>();
     _focusNode = FocusNode()
       ..addListener(() {
         // 触发刷新
         setState(() {});
-        // Timer(Duration(seconds: 1), () {
-        //   if (_focusNode.hasFocus) _focusNode.unfocus();
-        // });
       });
+    _debounceKey = Symbol('SearchBar/handleKeyboardChangeDebounced');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (widget.autofocus) {
+      _focusOnTextField();
+    }
   }
 
   @override
   void dispose() {
     // Clean up the focus node when the Form is disposed.
     _focusNode.dispose();
+    // 取消键盘的监听
+    WidgetsBinding.instance!.removeObserver(this);
     super.dispose();
+  }
+
+  @override
+  void didChangeMetrics() {
+    if (!widget.enabled) {
+      return;
+    }
+    final currentKeyboardBottom = MediaQuery.of(context).viewInsets.bottom;
+    // 如果键盘底部距离没有发生变化, 不作处理
+    if (currentKeyboardBottom == _lastKeyboardBottom) {
+      return;
+    }
+    final currentKeyboardOpen = currentKeyboardBottom > _lastKeyboardBottom;
+    // 如果跟上次一样的话吗不需要重新触发回调
+    if (currentKeyboardOpen != _lastIsKeyboardOpen) {
+      _lastIsKeyboardOpen = currentKeyboardOpen;
+      _handleKeyboardChange(currentKeyboardOpen
+          ? SearchBarKeyboardStatus.opening
+          : SearchBarKeyboardStatus.closing);
+    }
+    _lastKeyboardBottom = currentKeyboardBottom;
+
+    // 这里一直触发防抖回调, 只有最后一次才会触发, 所以代码动画已完成
+    _handleKeyboardChangeDebounced(currentKeyboardOpen
+        ? SearchBarKeyboardStatus.opened
+        : SearchBarKeyboardStatus.closed);
   }
 
   @override
@@ -112,5 +175,27 @@ class _SearchBarState extends State<SearchBar> {
     if (ModalRoute.of(context)!.settings.name != '/search') {
       Navigator.of(context).pushNamed('/search');
     }
+  }
+
+  // 异步的聚焦输入框, 不然会跟 Hero 动画冲突
+  void _focusOnTextField() async {
+    await Future.delayed(
+      Duration(milliseconds: AppTheme.baseAnimationDuration),
+    );
+    if (!_focusNode.hasFocus) _focusNode.requestFocus();
+  }
+
+  // 键盘更改回调封装
+  void _handleKeyboardChange(SearchBarKeyboardStatus keyboardStatus) {
+    if (widget.handleKeyboardChange == null) {
+      return;
+    }
+    widget.handleKeyboardChange!(keyboardStatus);
+  }
+
+  // 键盘更改回调防抖封装
+  void _handleKeyboardChangeDebounced(SearchBarKeyboardStatus keyboardStatus) {
+    Debounce.debounce(_debounceKey, Duration(milliseconds: 50),
+        () => _handleKeyboardChange(keyboardStatus));
   }
 }
