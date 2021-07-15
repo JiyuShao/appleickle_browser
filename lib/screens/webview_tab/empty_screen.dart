00db4a4cc437b790f79ef2f38881975c5b6b8d38
Jiyu Shao <jiyu.shao@gmail.com>
2021-07-15 18:00:29 +0800

    feat: optimize webview_screen to search_screen animation

--- lib/screens/webview_tab/empty_screen.dart
+++ lib/screens/webview_tab/empty_screen.dart
@@ -3,9 +3,10 @@
  * @Author: Jiyu Shao 
  * @Date: 2021-06-29 17:53:00 
  * @Last Modified by: Jiyu Shao
- * @Last Modified time: 2021-07-12 10:36:02
+ * @Last Modified time: 2021-07-15 16:54:23
  */
 import 'package:appleickle_browser/screens/popup_menu/popup_menu_screen.dart';
+import 'package:appleickle_browser/screens/search/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:appleickle_browser/models/app_theme_model.dart';
 import 'package:appleickle_browser/screens/popup_menu/popup_menu_hero.dart';
@@ -16,6 +17,7 @@ import 'package:appleickle_browser/widgets/bottom_bar/bottom_bar.dart'
     as bottom_bar;
 import 'package:appleickle_browser/widgets/page_scaffold/page_scaffold.dart';
 import 'package:appleickle_browser/widgets/search_bar/search_bar.dart';
+import 'package:provider/provider.dart';
 
 class EmptyScreen extends StatefulWidget {
   // Hero 动画相关 tag
@@ -76,8 +78,10 @@ class _EmptyScreenState extends State<EmptyScreen>
 
   @override
   Widget build(BuildContext context) {
+    final appThemeModel = Provider.of<AppThemeModel>(context, listen: true);
     final mq = MediaQuery.of(context);
     final topOffset = mq.size.height / 3;
+
     return PageScaffold(
       bottomArea: bottom_bar.BottomBar(
         bottomBarItemList: bottomBarItemList,
@@ -89,7 +93,12 @@ class _EmptyScreenState extends State<EmptyScreen>
             return SizedBox();
           } else {
             return Container(
-              padding: EdgeInsets.fromLTRB(20, topOffset, 20, 20),
+              padding: EdgeInsets.fromLTRB(
+                appThemeModel.basePagePadding,
+                topOffset,
+                appThemeModel.basePagePadding,
+                appThemeModel.basePagePadding,
+              ),
               alignment: Alignment.topCenter,
               child: SearchHero(
                 heroTag: widget.heroTag,
@@ -101,13 +110,15 @@ class _EmptyScreenState extends State<EmptyScreen>
                   BuildContext toHeroContext,
                 ) {
                   return SearchBar(
-                    heroTag: widget.heroTag,
+                    searchScreenArguments:
+                        SearchScreenArguments(heroTag: widget.heroTag),
                     enabled: false,
                     autofocus: false,
                   );
                 },
                 child: SearchBar(
-                  heroTag: widget.heroTag,
+                  searchScreenArguments:
+                      SearchScreenArguments(heroTag: widget.heroTag),
                   enabled: false,
                   autofocus: false,
                 ),
--- lib/utils/routes/page_routes/search_page_route.dart
+++ lib/utils/routes/page_routes/search_page_route.dart
@@ -1,3 +1,4 @@
+import 'package:appleickle_browser/models/app_theme_model.dart';
 import 'package:flutter/material.dart';
 import 'package:appleickle_browser/utils/routes/page_routes/common_page_route_builder.dart';
 
@@ -12,6 +13,9 @@ class SearchPageRoute<T> extends CommonPageRouteBuilder<T> {
           maintainState: true,
           opaque: false,
           barrierDismissible: true,
+          transitionDuration: Duration(
+            milliseconds: AppThemeModel.baseAnimationDuration * 2,
+          ),
         );
 
   // 页面生成器
